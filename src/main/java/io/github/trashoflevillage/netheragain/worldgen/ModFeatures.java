bd0443a0f5e27833904d32656d5fdca3e223e1f0
trashoflevillage <uhhnokthxbye@gmail.com>
2025-02-20 10:49:50 -0500

    Finalized Prismite Spike feature generation

--- src/main/java/io/github/trashoflevillage/netheragain/worldgen/ModFeatures.java
+++ src/main/java/io/github/trashoflevillage/netheragain/worldgen/ModFeatures.java
@@ -1,16 +1,16 @@
 package io.github.trashoflevillage.netheragain.worldgen;
 
 import io.github.trashoflevillage.netheragain.NetherAgain;
-import io.github.trashoflevillage.netheragain.worldgen.features.PrismiteSpikeFeature;
+import io.github.trashoflevillage.netheragain.worldgen.features.CrystalSpikeFeature;
 import io.github.trashoflevillage.trashlib.initializers.FeatureInitializer;
-import net.minecraft.world.gen.feature.DefaultFeatureConfig;
 import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.SimpleBlockFeatureConfig;
 
 public class ModFeatures {
-    private static FeatureInitializer initializer = new FeatureInitializer(NetherAgain.MOD_ID);
+    private static final FeatureInitializer initializer = new FeatureInitializer(NetherAgain.MOD_ID);
 
-    public static Feature<DefaultFeatureConfig> PRISMITE_SPIKE = initializer.register(
-            "prismite_spike", new PrismiteSpikeFeature(DefaultFeatureConfig.CODEC)
+    public static final Feature<SimpleBlockFeatureConfig> CRYSTAL_SPIKE = initializer.register(
+            "crystal_spike", new CrystalSpikeFeature(SimpleBlockFeatureConfig.CODEC)
     );
 
     public static void register() {}
